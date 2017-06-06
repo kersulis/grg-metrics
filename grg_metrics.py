fb39817c9ff46b6e8475ad25bbba365d3ed4f76a
Jonas Kersulis <kersulis@umich.edu>
2017-06-06 13:58:51 -0400

    Docfix

--- grg_metrics.py
+++ grg_metrics.py
@@ -3,11 +3,10 @@ import grg_grgdata
 import networkx as nx
 import numpy as np
 
-"""
-Recursively walk grg components.
-Code taken from Carleton Coffrin.
-"""
 def walk_components(grg_data):
+    """Recursively walk grg components.
+    Code taken from Carleton Coffrin.
+    """
     for key, value in grg_data.items():
         yield key, value
         if isinstance(value, dict):
@@ -16,40 +15,39 @@ def walk_components(grg_data):
                     for nested_key, nested_value in walk_components(value[key]):
                         yield nested_key, nested_value
 
-"""
-Given a GRGv1.0 JSON document, return a networkx graph.
-
-Properties embedded in the graph:
-- network
-    - id
-    - subtype
-    - per_unit
-    - description (optional)
-- bus
-    - type (can be 'bus', 'busbar', or 'logical_bus')
-    - id
-    - link (only for 'busbar' and 'logical_bus')
-    - voltage
-- ac_line
-    - id
-    - voltage_level_1_id
-    - voltage_level_2_id
-    - shunt_1
-    - shunt_2
-    - impedance
-    - current_limits_1
-    - current_limits_2
-- two_winding_transformer
-    - id
-    - voltage_level_1_id
-    - voltage_level_2_id
-
-Notes:
-- 'bus', 'busbar', and 'logical_bus' are all considered buses.
-- Edges are taken from both 'ac_line' and 'two_winding_transformer' objects.
-- Unlike iGRG, GRG has no 'status' field, so all buses and lines are included.
-"""
 def grg2nx(data):
+    """Given a GRGv1.0 JSON document, return a networkx graph.
+
+    Properties embedded in the graph:
+    - network
+        - id
+        - subtype
+        - per_unit
+        - description (optional)
+    - bus
+        - type (can be 'bus', 'busbar', or 'logical_bus')
+        - id
+        - link (only for 'busbar' and 'logical_bus')
+        - voltage
+    - ac_line
+        - id
+        - voltage_level_1_id
+        - voltage_level_2_id
+        - shunt_1
+        - shunt_2
+        - impedance
+        - current_limits_1
+        - current_limits_2
+    - two_winding_transformer
+        - id
+        - voltage_level_1_id
+        - voltage_level_2_id
+
+    Notes:
+    - 'bus', 'busbar', and 'logical_bus' are all considered buses.
+    - Edges are taken from both 'ac_line' and 'two_winding_transformer' objects.
+    - Unlike iGRG, GRG has no 'status' field, so all buses and lines are included.
+    """
     G = nx.Graph()
 
     # embed these properties into networkx graph
